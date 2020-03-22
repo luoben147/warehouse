73f38e6210109bdd3905a426815243cda44ea4cf
luoben_147 <315523376@qq.com>
2020-03-22 17:08:54 +0800

    '完成菜单管理CRUD'

--- src/main/java/com/luoben/warehouse/sys/common/TreeNode.java
+++ src/main/java/com/luoben/warehouse/sys/common/TreeNode.java
@@ -16,7 +16,7 @@ public class TreeNode {
     private String href;
     private Boolean spread;
     private List<TreeNode> children = new ArrayList<TreeNode>();
-    private String checkArr;
+    //private String checkArr;
 
     /**
      * 首页左边导航树的构造器
@@ -51,12 +51,12 @@ public class TreeNode {
      * @param spread
      * @param checkArr
      */
-    public TreeNode(Integer id, Integer pid, String title, Boolean spread, String checkArr) {
-        super();
-        this.id = id;
-        this.pid = pid;
-        this.title = title;
-        this.spread = spread;
-        this.checkArr = checkArr;
-    }
+//    public TreeNode(Integer id, Integer pid, String title, Boolean spread, String checkArr) {
+//        super();
+//        this.id = id;
+//        this.pid = pid;
+//        this.title = title;
+//        this.spread = spread;
+//        this.checkArr = checkArr;
+//    }
 }
